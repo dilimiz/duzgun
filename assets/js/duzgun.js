1a5febe677f1d019b51e652f51c3fdc4fbf7362e
haghrah <arslan.haghrah@gmail.com>
2022-08-30 19:44:28 +0430

    Editing site ...

--- assets/js/duzgun.js
+++ assets/js/duzgun.js
@@ -2,7 +2,7 @@ function duzgun() {
     var inputText = document.getElementById('input').value;
     wordList = inputText.split(" ");
     var outputText = '';
-    for(var i=0;i<length(wordList);i++) {
+    for(var i=0;i<wordList.length;i++) {
         outputText += wordList[i] + '\n';
     }
     document.getElementById('output').value = outputText;
